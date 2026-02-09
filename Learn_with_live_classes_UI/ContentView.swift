fcda71b73a6606bf767a5720374bb36eb732c743
Om Kashinath Makode <omkashinath_m@nexteducation.in>
2026-02-09 11:35:01 +0530

    Initial Commit

---
+++ Learn_with_live_classes_UI/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Learn_with_live_classes_UI
+//
+//  Created by Sai Krishna on 2/9/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Learn_with_live_classes_UI/Learn_with_live_classes_UIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Learn_with_live_classes_UIApp.swift
+//  Learn_with_live_classes_UI
+//
+//  Created by Sai Krishna on 2/9/26.
+//
+
+import SwiftUI
+
+@main
+struct Learn_with_live_classes_UIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
